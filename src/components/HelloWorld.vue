2a19239ccf5a19c718079726be4aaff0d370fa02
sarahzxwy <sarahab703@gmail.com>
2026-04-10 21:52:19 -0300

    initialize the project 🚀

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center text-white">
+
+    <h1 class="text-2xl font-bold text-blue-400 mb-6">
+      Hello world 
+    </h1>
+
+  </div>
+</template>
